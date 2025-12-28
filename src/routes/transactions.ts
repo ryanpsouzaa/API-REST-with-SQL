8bfe05cdd970f604ed771970901889f35619e6e1
ryanpsouza <ryanpsouzagit@gmail.com>
2025-12-28 18:42:02 -0300

    feat: implementando consultas e resumo das transacoes do modulo transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -6,27 +6,55 @@ import { randomUUID } from 'node:crypto';
 export async function transactionsRoutes(app: FastifyInstance) {
   app.post('/', async (request, reply) => {
 
-    const transactionsSchema = z.object({
+    const createBodyTransactionsSchema = z.object({
       title: z.string(),
       amount: z.number(),
       type: z.enum(['credit', 'debit']),
     });
 
-    const { title, amount, type } = transactionsSchema.parse(request.body);
+    const { title, amount, type } = createBodyTransactionsSchema.parse(request.body);
 
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+
     });
 
-
     return reply.code(201).send();
+  });
+
+  app.get('/:id', async (request, reply) => {
+    try {
+      const getTransactionParamsSchema = z.object({
+        id: z.uuid({ version: 'v4' }),
+      });
+
+      const { id } = getTransactionParamsSchema.parse(request.params);
+
+      const transactionFind = await knex('transactions').where('id', id).first();
+
+      return reply.code(200).send({
+        transaction: transactionFind,
+      });
+
+    } catch(err){
+      console.error(err);
+      return reply.code(500).send({message: 'Internal Server Error'});
+    }
   });
 
   app.get('/', async (request, reply) => {
     const transactions = await knex('transactions').select('*');
 
     return reply.code(200).send(transactions);
+  });
+
+  app.get('/summary', async (request, reply) => {
+    const summary = await knex('transactions').sum('amount', {as: 'amount'}).first();
+
+    reply.code(200).send({
+      summary,
+    });
   });
 }
